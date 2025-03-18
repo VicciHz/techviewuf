79745aa87e45a301539262a9b1dfe0ad0d38945d
Vicci <victor.fox.hz@gmail.com>
2025-03-18 09:10:53 +0100

    update

--- app/components/loginButton.tsx
+++ app/components/loginButton.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function LoginButton() {
     return (
-        <Link href="./login">
+        <Link href="../login">
             <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition">
                 Go to Login
             </button>
